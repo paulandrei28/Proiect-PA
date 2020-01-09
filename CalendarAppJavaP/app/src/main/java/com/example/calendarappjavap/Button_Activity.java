0278eb81c9bf3a96a2fe4966ba97be78a7cfeb15
mihai_rusu999 <mihai_rusu999@yahoo.com>
2020-01-09 13:27:57 +0200

    Adaugare buton

---
+++ CalendarAppJavaP/app/src/main/java/com/example/calendarappjavap/Button_Activity.java
@@ -0,0 +1,15 @@
+package com.example.calendarappjavap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Button_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_);
+    }
+}
--- CalendarAppJavaP/app/src/main/java/com/example/calendarappjavap/MainActivity.java
+++ CalendarAppJavaP/app/src/main/java/com/example/calendarappjavap/MainActivity.java
@@ -3,19 +3,33 @@ package com.example.calendarappjavap;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     CalendarView calendarView;
     TextView myDate;
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opennewfile();
+            }
+        });
+
 
         calendarView = (CalendarView) findViewById(R.id.calendarView);
         myDate = (TextView) findViewById(R.id.myDate);
@@ -25,7 +39,12 @@ public class MainActivity extends AppCompatActivity {
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
                 String date = day + "/" + (month+1) + "/" + year;
                 myDate.setText(date);
-            }
+                }
+
         });
+    }
+    public void opennewfile() {
+        Intent intent = new Intent(this, Button_Activity.class);
+        startActivity(intent);
     }
 }
